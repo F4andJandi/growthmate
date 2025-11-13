57bf99b110d6b1ef631a3d29c7ef8991855f1425
Juyeong Moon <whoor2828@gmail.com>
2025-11-13 15:57:07 +0900

    feat: 필드 제약 조건 추가

--- src/main/java/com/wanted/growthmate/learning/course/domain/dto/CourseCreateRequest.java
+++ src/main/java/com/wanted/growthmate/learning/course/domain/dto/CourseCreateRequest.java
@@ -1,5 +1,7 @@
 package com.wanted.growthmate.learning.course.domain.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,13 +12,18 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor // 테스트에서 필요함.
 public class CourseCreateRequest {
+    @NotNull(message = "title은 필수 입력 값입니다.")
     private String title;
+
+    @NotNull(message = "categoryId는 필수 입력 값입니다.")
     private Long categoryId;
     private String description;
     private Long userId; // 강사ID
+
+    @NotNull(message = "pointAmount는 필수 입력 값입니다.")
+    @Positive(message = "pointAmount는 0보다 큰 값이어야 합니다.")
     private Long pointAmount;
     private String imageUrl;
 
     public CourseCreateRequest() {}
-
 }
